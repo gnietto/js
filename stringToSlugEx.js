757f04750bc256c830e5346136cf6fb22cd42318
gnietto <47994713+gnietto@users.noreply.github.com>
2020-03-04 17:25:01 -0300

    mejora la solución string to slug

--- stringToSlugEx.js
+++ stringToSlugEx.js
@@ -3,8 +3,10 @@
 var globalTitle = "Winter Is Coming";
 
 function urlSlug(title) {
-    return title.toLowerCase().trim().split(' ').join('-');
+    return title.toLowerCase().trim().split(/\s+/).join('-');
 }
 
 let winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
 console.log(winterComing);
+
+//recordar que la regexp /\s+/ significa "encontrar el caracter 'espacio en blanco' cuando presenta 1 ocurrencia o más (+)"
