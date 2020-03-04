df2212deaa2f56e432fac14a212d52ec85c66478
gnietto <47994713+gnietto@users.noreply.github.com>
2020-03-04 17:45:35 -0300

    agrega archivo que resuelve project euler #1: multiplos de 3 y 5. Se delinea el algoritmo

---
+++ eulerMultiplesOf3and5.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function multiplesOf3and5(number) {
+  // un número es multiplo de 3 si la suma de sus dígitos es exactamente divisible por 3
+  // un número es multiplo de 5 si el número termina en 0 o 5
+  // definir números múltiplos de 3 que son inferiores al argumento 'number'
+  // definir números múltiplos de 5 que son inferiores al argumento 'number'
+  // definir elementos comunes a ambos cálculos anteriores
+  // retornar la suma de todos los números comunes resultantes
+
+}
+
+multiplesOf3and5(1000);
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
