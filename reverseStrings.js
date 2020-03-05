613a8de3c20be52240c0bb58f15dc7f7748c3ad5
gnietto <47994713+gnietto@users.noreply.github.com>
2020-03-05 14:11:15 -0300

    agrega archivo acerca de revertir un string

---
+++ reverseStrings.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("Howdy"));
+console.log(reverseString("Greetings from Earth"));
