845a137a7c46ac9dc0fc263e79a38b8d2875ddfb
gnietto <47994713+gnietto@users.noreply.github.com>
2020-03-12 14:05:41 -0300

    se agrega archivos con soluciones parciales y/o mejores comentarios de los ejercicios

---
+++ stackClassEx.js
@@ -0,0 +1,4 @@
+
+
+
+//In the last section, we talked about what a stack is and how we can use an array to represent a stack. In this section, we will be creating our own stack class. Although you can use arrays to create stacks, sometimes it is best to limit the amount of control we have with our stacks. Apart from the push and pop method, stacks have other useful methods. Let's add a peek, isEmpty, and clear method to our stack class.
---
+++ sumAll.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function sumAll(arr) {
+	return arr.filter()
+}
+
+console.log(sumAll([1, 4]));
+console.log(sumAll([4, 1]));
+console.log(sumAll([5, 10]));
+console.log(sumAll([10, 5]));
+
+//We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+//aplanar el array
+//encontrar los 4 primeros números más grandes
+//retornar array con esos números
